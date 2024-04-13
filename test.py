6f95f0cd5563c624166458ad17e1aaf01f5c9a20
snakeJ11 <135451132+snakeJ11@users.noreply.github.com>
2024-04-13 10:04:36 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+# main.py
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    n1 = float(input("Ingrese el primer número: "))
+    n2 = float(input("Ingrese el segundo número: "))
+    print("La suma es:", sumar(n1, n2))
